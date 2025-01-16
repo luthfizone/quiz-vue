d8b21069f02c061bc32de3025ae68bf52d90b7c0
luthfizone <luthfizonedev@gmail.com>
2025-01-16 21:44:28 +0700

    feat: header ui

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <header>
+      <h1 id="title">Quiz Apps</h1>
+      <input
+        type="search"
+        name="search-input"
+        id="search-input"
+        placeholder="Search Quiz..."
+      />
+    </header>
   </main>
 </template>
 
 <style scoped>
+main {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
 header {
-  line-height: 1.5;
+  margin: 50px 0;
+  display: flex;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-right: 30px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#search-input {
+  padding: 10px;
+  background-color: #c9c9c9a9;
+  border: none;
+  border-radius: 5px;
 }
 </style>
